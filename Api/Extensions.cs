59aa592797abef1a50af879c3548ff0e36f3e9d6
Peter Popov <p.dim.popov@gmail.com>
2022-01-31 08:28:33 +0200

    refactor(Program): simplify setup expressions

---
+++ Api/Extensions.cs
@@ -0,0 +1,108 @@
+using System.Text;
+using Api.Helpers.Authorization;
+using Data.DbContexts;
+using Helpers;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Runners;
+using Runners.Abstractions;
+
+namespace Api;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddGradableAppProvider(this IServiceCollection services) =>
+        services
+            .AddHttpContextAccessor()
+            .AddScoped<IApp>(serviceProvider =>
+            {
+                var routeValues = serviceProvider.GetRequiredService<IHttpContextAccessor>()
+                    .HttpContext
+                    !.Request
+                    .RouteValues;
+
+                string? GetProblemType() => routeValues["problemType"]?.ToString()?.ToLower();
+
+                var processStarter = serviceProvider.GetService<IProcessStarter>()!;
+                return routeValues["programmingLanguage"]?.ToString()?.ToLower() switch
+                {
+                    "javascript" => GetProblemType() switch
+                    {
+                        "single-file" => new JavaScriptSingleFileConsoleApp(processStarter),
+                        { } type => new NotSupportedApp {Type = type},
+                        _ => new NotSupportedApp(),
+                    },
+                    { } language => new NotSupportedApp {Language = language},
+                    _ => new NotSupportedApp(),
+                };
+            });
+
+    public static IServiceCollection AddAuth(this IServiceCollection services)
+    {
+        var jwtSection = services
+            .BuildServiceProvider()
+            .GetService<IConfiguration>()
+            !.GetSection("Jwt");
+        services.Configure<JwtSettings>(jwtSection);
+
+        var jwtSettings = jwtSection.Get<JwtSettings>();
+
+        services.AddScoped<IJwtUtils, JwtUtils>();
+
+        services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                var secret = jwtSettings.Secret;
+                options.SaveToken = true;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secret)),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                };
+            });
+
+        return services;
+    }
+
+    public static IServiceCollection AddConvenientDbContext(this IServiceCollection services, bool isDevelopment)
+        => isDevelopment
+            ? services.AddDbContext<AppDbContext, LocalDbContext>()
+            // TODO: add a real db at some point
+            : services;
+}
+
+public static class WebApplicationExtensions
+{
+    public static IApplicationBuilder UseClientSideAppDevelopmentServerWhen(
+        this IApplicationBuilder app,
+        bool condition
+    ) => condition ? app.UseClientSideAppDevelopmentServer() : app;
+
+    public static IApplicationBuilder UseClientSideAppDevelopmentServer(this IApplicationBuilder app) =>
+        app.MapWhen(x => x.Connection.LocalPort == 3003, app0 =>
+        {
+            app0.UseSpa(spa =>
+            {
+                spa.Options.PackageManagerCommand = "yarn";
+                spa.Options.SourcePath = "ClientApp";
+                spa.Options.DevServerPort = 3003;
+                spa.UseReactDevelopmentServer("dev");
+            });
+        });
+
+    public static IApplicationBuilder UseAuth(this IApplicationBuilder app) =>
+        app.UseAuthentication().UseAuthorization();
+
+    public static IApplicationBuilder UseSwaggerDocsWhen(this IApplicationBuilder app, bool condition)
+        => condition
+            ? app.UseSwagger().UseSwaggerUI()
+            : app;
+}
--- Api/Program.cs
+++ Api/Program.cs
@@ -1,13 +1,6 @@
-using System.Text;
-using Api.Helpers.Authorization;
+using Api;
 using Api.Services;
-using Data.DbContexts;
 using Helpers;
-using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
-using Runners;
-using Runners.Abstractions;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -44,87 +37,18 @@ builder.Services.AddSwaggerGen(swaggerGenOptions =>
     });
 });
 
-// TODO: add a real db at some point
-if (builder.Environment.IsDevelopment())
-{
-    builder.Services.AddDbContext<AppDbContext, LocalDbContext>();
-}
-
-builder.Services.AddHttpContextAccessor();
-
-builder.Services.AddTransient<IProcessStarter, ProcessStarter>();
-builder.Services.AddScoped<IApp>(serviceProvider =>
-{
-    var routeValues = serviceProvider.GetRequiredService<IHttpContextAccessor>()
-        .HttpContext
-        !.Request
-        .RouteValues;
-
-    string? GetProblemType() => routeValues["problemType"]?.ToString()?.ToLower();
-
-    var processStarter = serviceProvider.GetService<IProcessStarter>()!;
-    return routeValues["programmingLanguage"]?.ToString()?.ToLower() switch
-    {
-        "javascript" => GetProblemType() switch
-        {
-            "single-file" => new JavaScriptSingleFileConsoleApp(processStarter),
-            { } type => new NotSupportedApp { Type = type },
-            _ => new NotSupportedApp(),
-        },
-        { } language => new NotSupportedApp { Language = language },
-        _ => new NotSupportedApp(),
-    };
-});
-
-builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("Jwt"));
-
-builder.Services.AddScoped<IJwtUtils, JwtUtils>();
-builder.Services.AddScoped<IUserService, UserService>();
-
 builder.Services
-    .AddAuthentication(options =>
-    {
-        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-    })
-    .AddJwtBearer(options =>
-    {
-        var secret = builder.Configuration.GetSection("Jwt").Get<JwtSettings>().Secret;
-        options.SaveToken = true;
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secret)),
-            ValidateIssuer = false,
-            ValidateAudience = false,
-        };
-    });
+    .AddConvenientDbContext(builder.Environment.IsDevelopment())
+    .AddTransient<IProcessStarter, ProcessStarter>()
+    .AddGradableAppProvider()
+    .AddScoped<IUserService, UserService>()
+    .AddAuth();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-
-    app.MapWhen(x => x.Connection.LocalPort == 3003, app0 =>
-    {
-        app0.UseSpa(spa =>
-        {
-            spa.Options.PackageManagerCommand = "yarn";
-            spa.Options.SourcePath = "ClientApp";
-            spa.Options.DevServerPort = 3003;
-            spa.UseReactDevelopmentServer("dev");
-        });
-    });
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthentication();
-app.UseAuthorization();
+app.UseSwaggerDocsWhen(app.Environment.IsDevelopment())
+    .UseClientSideAppDevelopmentServerWhen(app.Environment.IsDevelopment())
+    .UseHttpsRedirection()
+    .UseAuth();
 
 app.MapControllers();
-
 app.Run();
